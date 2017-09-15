c94da3f1255bb7556cb7183709bf098a4d48595b
Lina Lekova <lina.lekova@gmail.com>
2017-09-15 18:55:54 -0400

    Readme added and default controller deleted

--- MyCodeCamp/DbUtilities/CampContext.cs
+++ MyCodeCamp/DbUtilities/CampContext.cs
@@ -1,50 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using MyCodeCamp.Entities;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;using Microsoft.EntityFrameworkCore;using Microsoft.Extensions.Configuration;using MyCodeCamp.Entities;namespace MyCodeCamp.DbUtilities{    public class CampContext : IdentityDbContext    {        private IConfiguration _config;        public CampContext(DbContextOptions options, IConfiguration config)          : base(options)        {            _config = config;        }        public DbSet<Camp> Camps { get; set; }        public DbSet<Speaker> Speakers { get; set; }        public DbSet<Talk> Talks { get; set; }        protected override void OnModelCreating(ModelBuilder builder)        {            base.OnModelCreating(builder);            builder.Entity<Camp>()                .Property(c => c.Moniker)                .IsRequired();
 
-namespace MyCodeCamp.DbUtilities
-{
-    public class CampContext : IdentityDbContext
-    {
-        private IConfiguration _config;
+            builder.Entity<Camp>()                .Property(c => c.RowVersion)                .ValueGeneratedOnAddOrUpdate()                .IsConcurrencyToken();
 
-        public CampContext(DbContextOptions options, IConfiguration config)
-          : base(options)
-        {
-            _config = config;
-        }
+            builder.Entity<Speaker>()                .Property(c => c.RowVersion)                .ValueGeneratedOnAddOrUpdate()                .IsConcurrencyToken();
 
-        public DbSet<Camp> Camps { get; set; }
-        public DbSet<Speaker> Speakers { get; set; }
-        public DbSet<Talk> Talks { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<Camp>()
-              .Property(c => c.Moniker)
-              .IsRequired();
-            builder.Entity<Camp>()
-              .Property(c => c.RowVersion)
-              .ValueGeneratedOnAddOrUpdate()
-              .IsConcurrencyToken();
-            builder.Entity<Speaker>()
-              .Property(c => c.RowVersion)
-              .ValueGeneratedOnAddOrUpdate()
-              .IsConcurrencyToken();
-            builder.Entity<Talk>()
-              .Property(c => c.RowVersion)
-              .ValueGeneratedOnAddOrUpdate()
-              .IsConcurrencyToken();
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseSqlServer(_config["Data:ConnectionString"]);
-        }
-    }
-}
+            builder.Entity<Talk>()                .Property(c => c.RowVersion)                .ValueGeneratedOnAddOrUpdate()                .IsConcurrencyToken();        }        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)        {            base.OnConfiguring(optionsBuilder);            optionsBuilder.UseSqlServer(_config["Data:ConnectionString"]);        }    }}
